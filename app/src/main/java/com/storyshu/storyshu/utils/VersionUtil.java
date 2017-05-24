b598fef0366e3c758c313687605d6a77abea480d
luoming <luomingbear@163.com>
2017-05-25 03:11:48 +0800

    新增：0.02测试版

--- app/src/main/java/com/storyshu/storyshu/utils/VersionUtil.java
+++ app/src/main/java/com/storyshu/storyshu/utils/VersionUtil.java
@@ -6,5 +6,5 @@ package com.storyshu.storyshu.utils;
  */
 
 public class VersionUtil {
-    public static final float version = 0.019f; //当前的软件版本号
+    public static final float version = 0.02f; //当前的软件版本号
 }
